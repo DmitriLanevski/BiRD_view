c5949e01e20bf71293c19244224bae6370cae463
vpinja <35135787+vpinja@users.noreply.github.com>
2019-03-22 15:31:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 import base64
 
-from DataContainer import DataContainer
+from dataContainer import DataContainer
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -15,6 +15,10 @@ colors = {
     'background': '#111111',
     'text': '#000080'
 }
+
+uploaded_files = {}
+vis_lis = ['BRDF-visualization', 'Integrated', 'CIELAB visualization', 'Color difference calculation']
+var_lis = []
 
 def server_layout():
     layout = html.Div([
@@ -33,181 +37,217 @@ def server_layout():
                 }
         ),
 
-        html.Div(children='''
-            First step: Download data''',
-                 style={
-                     'textAlign': 'center',
-                     'color': colors['text']
-                 }
-        ),
+        html.Hr(),
+        html.Div([
+            html.Div([
+                html.H6('Add and remove data'),
 
-        dcc.Upload(
-            html.Button('Upload File'),
-            id='upload-data',
-            style={
-                'textAlign': 'center',
-                'marginTop': 25,
-            }
-        ),
+                html.Br([]),
 
-        html.Div(id='data-container', style={'display': 'none'}),
-        html.Div(id='x-selector1', style={'display': 'none'}),
-        html.Div(id='pol-selector', style={'display': 'none'}),
+                html.P("Upload new file by clicking Upload file button.\
+                       Remove file by selecting it from the list and clicking Remove file button."),
 
-        html.Div(id='menu1'),
-        html.Div(id='graph1')
+                dcc.Upload(
+                    html.Button('Upload File'),
+                    id='upload-data',
+                    style={
+                        'marginTop': 25,}
+                ),
+                html.Div(id='data-container'),
 
-    ], style={"padding-top": "20px"})
+                html.Br([]),
+
+                html.Button('Remove file', id='remove'),
+                html.Div(id='output-container-button',
+                         children='Select file and press remove'),
+
+            ], className='six columns'),
+
+            html.Div([
+                html.H6('Uploaded files'),
+                html.Br([]),
+
+                dcc.Dropdown(
+                    id='file-dropdown',
+                    options=[{'label': i, 'value': i} for i in uploaded_files],
+                    clearable=False,
+                ),
+            ], className='six columns')
+        ], className="row "),
+
+        html.Hr(),
+
+        html.Div([
+            html.Div([
+                html.H6('Visualization'),
+                html.P("Select desired visualization method from the list below."),
+
+                dcc.RadioItems(
+                    id='visualization-radio',
+                    options=[{'label': i, 'value': i} for i in vis_lis],
+                    value=vis_lis[0],
+                ),
+            ], className='six columns'),
+
+            html.Div([
+                html.H6('BRDF visualization'),
+                html.P("Select variable for BRDF visualization."),
+
+                dcc.Dropdown(
+                    id='variable-dropdown',
+                    options=[{'label': i, 'value': i} for i in var_lis],
+                ),
+
+                html.P("Select polarization for BRDF visualization."),
+                dcc.RadioItems(
+                    id='polarization',
+                    options=[{'label': i, 'value': i} for i in var_lis],
+                )
+
+            ], className='six columns')
+        ], className="row "),
+
+        html.Hr(),
+
+        dcc.Graph(id='graph')
+
+    ])
 
     return layout
 
 app.layout = server_layout()
 
-def menu(content):
+def parse_content(content, name):
     content_type, content_string = content.split(',')
     dc = DataContainer(base64.b64decode(content_string))
-    var = list(dc.data.keys())
+    global uploaded_files
+    uploaded_files[name] = dc
 
-    var.remove('values')
-    var.remove('key_variation')
-    key_variation = dc.data['key_variation']
-    var.remove(key_variation)
-    for i in var:
-        if len(dc.list_maker(i, 1)) < 2:
-            var.remove(i)
+    return html.Div(
+        html.Div('File uploaded'),
+    )
 
-    if 'polarization' in dc.data:
-        var.remove('polarization')
-        return html.Div([
-            html.Label('Second step: Select variable for x-axis. Variable of y axis is the key variation.',
-                       style={
-                           'textAlign': 'center',
-                           'color': colors['text'],
-                           'marginTop': 20
-                       }
-                       ),
-            dcc.Dropdown(
-                id='x-selector1',
-                options=[{'label':i, 'value':i} for i in var],
-                value=var[1],
-                clearable=False,
-                style={
-                    'marginLeft': 50,
-                    'marginRight': 50
-                }
-            ),
-            html.Label('Select polarization',
-                       style={
-                           'textAlign': 'center',
-                           'color': colors['text'],
-                           'marginTop': 20
-                       }
-                       ),
-            dcc.RadioItems(
-                id='pol-selector',
-                options=[
-                    {'label': 'S', 'value': 's'},
-                    {'label': 'P', 'value': 'p'},
-                ],
-                value='s',
-                style={
-                    'textAlign': 'center',
-                }
-            ),
-        ])
-    else:
-        return html.Div([
-            html.Label('Second step: Select variable for x-axis. Variable of y axis is the key variation.',
-                       style={
-                           'textAlign': 'center',
-                           'color': colors['text'],
-                           'marginTop': 20
-                       }
-                       ),
-            dcc.Dropdown(
-                id='x-selector1',
-                options=[{'label':i, 'value':i} for i in var],
-                value=var[1],
-                clearable=False,
-                style={
-                    'marginLeft': 50,
-                    'marginRight': 50
-                }
-            ),
-            html.Div(id='pol-selector', style={'display': 'none'}),
-        ])
-
-
-def make_graph(content, var, pol):
-    content_type, content_string = content.split(',')
-    dc = DataContainer(base64.b64decode(content_string))
+def brdf_graph(file, var, pol):
+    global uploaded_files
+    dc = uploaded_files[file]
     traces = []
     key_variation = dc.data['key_variation']
+    z = dc.data_by_key_variation(key_variation, pol)
 
     traces.append(go.Scatter3d(
-            z=dc.data_by_key_variation(key_variation, pol),
-            x=dc.x_axis_by_variable('theta_c', pol, len(dc.data_by_key_variation(key_variation,pol))),
-            y=dc.y_axis_by_key_variation(key_variation, len(dc.data_by_key_variation(key_variation, pol))),
+            z=z,
+            x=dc.x_axis_by_variable('theta_c', pol, len(z)),
+            y=dc.y_axis_by_key_variation(key_variation, len(z)),
 
             mode='markers',
             opacity=0.7,
             marker={
                 'size': 12,
-                'colorscale': 'Viridis',
                 'opacity': 0.7
             },
     ))
-    return html.Div([
-        dcc.Graph(id='angle-graph',
-                  figure={'data': traces,
-                          'layout': {
-                              'height': 800,
-                              'title': '3D graph',
-                              'xaxis': {
-                                  'title': var
-                              },
-                              'yaxis': {
-                                  'title': key_variation
-                              },
-                              'zaxis': {
-                                  'title': 'Measured value'
-                              }
-                          }
-                  }
-        ),
-        ])
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            hovermode='closest',
+            height=800,
+            title='BRDF visualization',
+        )
+    }
+
+def empty_graph():
+    traces = []
+    traces.append(go.Scatter(
+            x=(0,0,0),
+            y=(0,0,0),
+    ))
+    return {
+        'data': traces,
+    }
 
 @app.callback(
-    Output(component_id='data-container', component_property='children'),
-    [Input(component_id='upload-data', component_property='contents')]
+    Output('data-container', 'children'),
+    [Input('upload-data', 'contents')],
+    [State('upload-data', 'filename')]
 )
-def read_data(content):
-    if content is not None:
-        return content
-
-@app.callback(
-    Output(component_id='menu1', component_property='children'),
-    [Input(component_id='data-container', component_property='children')]
-)
-def create_menu(content):
+def read_data(content, name):
     if content is not None:
         children = [
-            menu(content)
+            parse_content(content, name)
         ]
         return children
 
 @app.callback(
-    Output(component_id='graph1', component_property='children'),
-    [Input(component_id='x-selector1', component_property='value'),
-     Input(component_id='pol-selector', component_property='value')],
-    [State(component_id='data-container', component_property='children')]
+    Output('file-dropdown', 'options'),
+    [Input('data-container', 'children'),
+     Input('output-container-button', 'children')]
 )
-def makeGraph(var, pol, content):
-    if content is not None:
-        children = [
-            make_graph(content, var, pol)
-        ]
-        return children
+def update_file_dropdown(value1, value2):
+    return[{'label': i, 'value': i} for i in uploaded_files]
+
+@app.callback(Output('output-container-button', 'children'),
+              [Input('remove', 'n_clicks')],
+              [State('file-dropdown', 'value')]
+)
+def remove_file(clicks, value):
+    global uploaded_files
+    if uploaded_files:
+        del uploaded_files[value]
+        return 'File "{}" removed from list'.format(value)
+
+@app.callback(Output('graph', 'figure'),
+              [Input('visualization-radio', 'value'),
+               Input('file-dropdown', 'value'),
+               Input('variable-dropdown', 'value'),
+               Input('polarization', 'value'),]
+)
+def update_graph(value, file, var, pol):
+    if value == 'BRDF-visualization' and file is not None:
+        return brdf_graph(file, var, pol)
+    elif value == 'Integrated' and file is not None:
+        print('Not yet implemented')
+    elif value == 'CIELAB visualization' and file is not None:
+        print('Not yet implemented')
+    elif value == 'Color difference calculation' and file is not None:
+        print('Not yet implemented')
+    else:
+        return empty_graph()
+
+@app.callback(Output('variable-dropdown', 'options'),
+              [Input('file-dropdown', 'value')]
+)
+def update_variable_dropdown(value):
+    global uploaded_files
+    if uploaded_files:
+        dc=uploaded_files[value]
+        var = list(dc.data.keys())
+        var.remove('values')
+        var.remove('key_variation')
+        key_variation = dc.data['key_variation']
+        var.remove(key_variation)
+        for i in var:
+            if len(dc.list_maker(i, 1)) < 2:
+                var.remove(i)
+
+        return [{'label': i, 'value': i} for i in var]
+    else:
+        return [{'label': i, 'value': i} for i in var_lis]
+
+@app.callback(Output('polarization', 'options'),
+              [Input('file-dropdown', 'value')]
+)
+def update_polarization(value):
+    global uploaded_files
+    if uploaded_files:
+        dc = uploaded_files[value]
+        if 'polarization' in dc.data:
+            options = [
+                {'label': 'S', 'value': 's'},
+                {'label': 'P', 'value': 'p'},
+            ]
+            return options
+
+    return [{'label': i, 'value': i} for i in var_lis]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
